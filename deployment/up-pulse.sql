17bd1e691200089b53c7ccd11f779de3ad2d4d1f
santosha2003 <santosha@svetan.ru>
2018-11-15 20:47:04 +0300

    ssd disk

--- deployment/up-pulse.sql
+++ deployment/up-pulse.sql
@@ -6,5 +6,4 @@ REPLACE INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES
 REPLACE INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('wallet', 'port', '20394', 'int', 'Monero Daemon RPC Wallet Port') ;
 REPLACE INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'coinCode', 'ETNXP', 'string', 'Coincode to be loaded up w/ the shapeshift getcoins argument.') ;
 REPLACE INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('pool', 'address_20393', '', 'string', 'Address to mine to for 20393 (ETNXP) port.');
-
-REPLACE INTO pool.users (username, pass, email, admin, payout_threshold) VALUES ('Administrator', 'x', 'root@santoshapro.me', 1, 0) ;
+REPLACE INTO pool.users (username, pass, email, admin, payout_threshold) VALUES ('Adm', '123', '123', 1, 0) ;
